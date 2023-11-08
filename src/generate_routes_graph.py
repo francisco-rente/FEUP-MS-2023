61f88bb7dd995a224e4e5db9defcb8da5143c961
adr1an0s0ar3s <adrianosoares35@gmail.com>
2023-11-08 11:34:25 +0000

    feat: clean datasets and add pos to nodes

--- src/generate_routes_graph.py
+++ src/generate_routes_graph.py
@@ -6,6 +6,10 @@ import pickle
 gtfs_directory = '../datasets/gtfs-stcp'
 feed = ptg.load_feed(gtfs_directory)
 routes = feed.routes['route_id']
+stops = feed.stops
+
+# Create a spatial layout based on stop coordinates
+pos = {stop_id: (lon, lat) for stop_id, lat, lon in zip(stops['stop_id'], stops['stop_lat'], stops['stop_lon'])}
 
 # Create a directed graph to represent the trips
 G = nx.DiGraph()
@@ -18,9 +22,13 @@ for _, route in routes.items():
     stop_times_direction_0 = feed.stop_times[feed.stop_times['trip_id'] == first_trip_direction_0['trip_id']]
     stop_sequence = stop_times_direction_0['stop_sequence'].values
     for i in range(len(stop_sequence) - 1):
-        from_stop = stop_times_direction_0[stop_times_direction_0['stop_sequence'] == stop_sequence[i]].iloc[0]
-        to_stop = stop_times_direction_0[stop_times_direction_0['stop_sequence'] == stop_sequence[i + 1]].iloc[0]
-        G.add_edge(from_stop['stop_id'], to_stop['stop_id'])
+        from_stop = stop_times_direction_0[stop_times_direction_0['stop_sequence'] == stop_sequence[i]].iloc[0]['stop_id']
+        to_stop = stop_times_direction_0[stop_times_direction_0['stop_sequence'] == stop_sequence[i + 1]].iloc[0]['stop_id']
+        if from_stop not in G.nodes:
+            G.add_node(from_stop, pos=pos[from_stop])
+        if to_stop not in G.nodes:
+            G.add_node(to_stop, pos=pos[to_stop])
+        G.add_edge(from_stop, to_stop)
 
     # Filter trips for direction_id == 1 and get the first trip
     try:
@@ -28,9 +36,13 @@ for _, route in routes.items():
         stop_times_direction_1 = feed.stop_times[feed.stop_times['trip_id'] == first_trip_direction_1['trip_id']]
         stop_sequence = stop_times_direction_1['stop_sequence'].values
         for i in range(len(stop_sequence) - 1):
-            from_stop = stop_times_direction_1[stop_times_direction_1['stop_sequence'] == stop_sequence[i]].iloc[0]
-            to_stop = stop_times_direction_1[stop_times_direction_1['stop_sequence'] == stop_sequence[i + 1]].iloc[0]
-            G.add_edge(from_stop['stop_id'], to_stop['stop_id'])
+            from_stop = stop_times_direction_1[stop_times_direction_1['stop_sequence'] == stop_sequence[i]].iloc[0]['stop_id']
+            to_stop = stop_times_direction_1[stop_times_direction_1['stop_sequence'] == stop_sequence[i + 1]].iloc[0]['stop_id']
+            if from_stop not in G.nodes:
+                G.add_node(from_stop, pos=pos[from_stop])
+            if to_stop not in G.nodes:
+                G.add_node(to_stop, pos=pos[to_stop])
+            G.add_edge(from_stop, to_stop)
     except:
         continue
 
