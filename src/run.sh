eb4e02ba5f8bc5afd178d06243f0f87ab4957f7a
adr1an0s0ar3s <adrianosoares35@gmail.com>
2023-12-08 22:10:07 +0000

    fix: change script to run_matrix.sh

--- src/run.sh
+++ src/run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 count=1
 for i in "$@"; do
-    qsub -l nodes=$i:ppn=2 -d . -l walltime=24:00:00 -F "$# ${count}" run_graph.sh
+    qsub -l nodes=$i:ppn=2 -d . -l walltime=24:00:00 -F "$# ${count}" run_matrix.sh
     (( count++ ))
 done
