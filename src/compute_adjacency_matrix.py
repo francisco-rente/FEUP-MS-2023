049c40c26410296c7d119b14b8d1226c55310473
adr1an0s0ar3s <adrianosoares35@gmail.com>
2023-12-08 19:00:34 +0000

    feat: modify scripts for Intel Dev Cloud; compute_adjacency_matrix.py now writes intermediate results to shortest_path.csv

--- src/compute_adjacency_matrix.py
+++ src/compute_adjacency_matrix.py
@@ -2,14 +2,14 @@ import pickle
 import os
 import pandas as pd
 import networkx as nx
-from concurrent.futures import ThreadPoolExecutor
+from concurrent.futures import ThreadPoolExecutor, as_completed
 import math
 from utils import timed
 
 
 @timed(__debug__)
-def read_graph_pickle(path):
-    return pickle.load(open(path, 'rb'))
+def read_graph_pickle(file_path):
+    return pickle.load(open(file_path, 'rb'))
 
 
 def isCentroid(node):
@@ -24,20 +24,17 @@ def extract_centroids_from_graph(G):
 def shortest_dijkstra_path(G, c1, c2):
     p = nx.dijkstra_path(G, c1, c2)
     d = sum([G[p[i]][p[i+1]]['weight'] for i in range(len(p)-1)])
-    return (c1, c2, '-'.join(p), d)
+    return (c1, c2, '-'.join(p), str(d))
 
 
 @timed(__debug__)
 def shortest_astar_path(G, c1, c2):
     p = nx.astar_path(G, c1, c2, heuristic=lambda x, y: math.dist(G.nodes[x]['pos'], G.nodes[y]['pos'])) 
     d = sum([G[p[i]][p[i+1]]['weight'] for i in range(len(p)-1)])
-    return (c1, c2, '-'.join(p), d)
+    return (c1, c2, '-'.join(p), str(d))
 
 
-def create_adjacency_matrix(G, centroids, num_paths=None):
-    shortest_path_df = pd.DataFrame(columns=['from', 'to', 'path', 'distance'])
-    shortest_path_arr = []
-    
+def create_adjacency_matrix_file(G, centroids, file_path, num_paths=None):
     with ThreadPoolExecutor() as executor:      # NOTE: It will default to the number of processors on the machine, multiplied by 5
 
         if num_paths:
@@ -56,20 +53,13 @@ def create_adjacency_matrix(G, centroids, num_paths=None):
         waiting = len(futures)
         completed = 0
         print(f'> Waiting for {waiting} results')
-        for future in futures:
-            if future.result() is not None:     # NOTE: It will wait indefinitely
-                shortest_path_arr.append(future.result())
+        with open(file_path, 'w') as f:
+            f.write('id,from,to,path,distance\n')
+            for completed_future in as_completed(futures):
                 completed += 1
+                f.write(f'{completed},{",".join(completed_future.result())}\n')     # NOTE: Flush is explicitly called after a newline
+                f.flush()
                 print(f'> Completed: {completed}/{waiting}')
-
-    shortest_path_df = pd.DataFrame(shortest_path_arr, columns=['from', 'to', 'path', 'distance'])
-    return shortest_path_df
-
-
-@timed(__debug__)
-def save_matrix_file(path, matrix):
-    print(f'> Saving matrix to {path}')
-    matrix.to_csv(path, index=False)
     
 
 def main():     # NOTE: Run python script with -O flag to disable DEBUG features (such as timed decorator)
@@ -80,9 +70,7 @@ def main():     # NOTE: Run python script with -O flag to disable DEBUG features
     G = read_graph_pickle('graph.gpickle')
     
     centroids = extract_centroids_from_graph(G) 
-    matrix = create_adjacency_matrix(G, centroids, num_paths=10 if __debug__ else None)
-
-    save_matrix_file('shortest_path.csv', matrix)
+    create_adjacency_matrix_file(G, centroids, 'shortest_path.csv', num_paths=20 if __debug__ else None)
 
 
 if __name__ == '__main__':
---
+++ src/run_matrix.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+source venv/bin/activate
+python -O compute_adjacency_matrix.py
