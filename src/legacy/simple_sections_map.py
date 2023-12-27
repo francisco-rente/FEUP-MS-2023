6a6c2731b76c2c6c14e119cf97b675098d7b5f84
francisco-rente <up201906761@up.pt>
2023-12-27 17:31:23 +0000

    feat: sections heatmap now includes indicator from validation csv

--- src/legacy/simple_sections_map.py
+++ src/legacy/simple_sections_map.py
@@ -2,8 +2,14 @@
 import geopandas as gpd
 import contextily as ctx
 import matplotlib.pyplot as plt
+import pandas as pd
 
-SECTIONS_GPKG = "../datasets/BGRI2021_1312.gpkg"
+SECTIONS_GPKG = "../../datasets/BGRI2021_1312.gpkg"
+
+
+# section_id,validations,section_id,weight,indicator
+VALIDATIONS = "./validation_per_section.csv"
+
 
 info_code = ['OBJECTID', 'BGRI2021',    
        'N_EDIFICIOS_CLASS_CONST_3_OU_MAIS_ALOJAMENTOS',
@@ -20,19 +26,32 @@ info_code = ['OBJECTID', 'BGRI2021',
        'N_INDIVIDUOS_15_24', 'N_INDIVIDUOS_25_64', 'N_INDIVIDUOS_65_OU_MAIS',
        'SHAPE_Area', 'geometry']
 
-def main():
-    # read the sections
-    df = gpd.read_file(SECTIONS_GPKG)
-    metric = 'N_INDIVIDUOS_0_14 / N_INDIVIDUOS'
 
+
+def read_datasets():
+    val_per_section = pd.read_csv(VALIDATIONS, sep=",")
+    df = gpd.read_file(SECTIONS_GPKG)
+
+
+    common_ids = len(set(df.OBJECTID).intersection(set(val_per_section["section_id"])))
+    print(f"Number of common ids: {common_ids}")
+
+    return df.merge(val_per_section, left_on='OBJECTID', right_on='section_index')
+
+def plot_map(df, metric):
     df = df.to_crs(epsg=3857)
-    ax = df.eval('pct_afam = N_INDIVIDUOS_0_14 / N_INDIVIDUOS')\
-        .plot('pct_afam', cmap='plasma', alpha=.7, linewidth=.25, edgecolor='k', legend=True)
+    ax = df.eval(f'pct_afam = {metric}')\
+            .plot('pct_afam', cmap='plasma', alpha=.7, linewidth=.25, edgecolor='k', legend=True)
     ax.set_axis_off()
     ax.set_title("Heatmap of " + metric)
 
     ctx.add_basemap(ax, source=ctx.providers.CartoDB.Positron)
     plt.show()
+
+
+def main():
+    df = read_datasets()
+    plot_map(df, "indicator/ N_INDIVIDUOS_25_64")
 
 if __name__ == '__main__':
     main()
