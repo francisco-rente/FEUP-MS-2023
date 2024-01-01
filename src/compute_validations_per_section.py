a98d6439286213e1ac3f4fb364acd41f20362f50
adr1an0s0ar3s <adrianosoares35@gmail.com>
2024-01-01 15:09:31 +0000

    feat: validations now take into account adjacent sections to rule out some that didn't have any

--- src/compute_validations_per_section.py
+++ src/compute_validations_per_section.py
@@ -16,22 +16,29 @@ def match_list_names(names_list, match_list):
 
 
 def group_validations_centroids(sections_df, validations_df, stops_df):
-
     validations_df['stop_name'] = match_list_names(validations_df['stop_name'], stops_df['stop_name']).values()
-
     stops_df['count'] = stops_df.groupby('stop_name')['stop_name'].transform('count')
-
     stops_df = stops_df.merge(validations_df, on='stop_name', how='left')
- 
+    
     centroid_validations_df = pd.DataFrame(columns=['section_id', 'validations'])
 
     for _, section_row in sections_df.iterrows():
         region_id = str(section_row['OBJECTID'])
         validations_count = 0
+
+        # Iterate over the current section and its adjacent sections
         for _, stop_row in stops_df.iterrows():
             stop = Point(stop_row['stop_lon'], stop_row['stop_lat'])
             if stop.within(section_row['geometry']):
                 validations_count += stop_row['validations'] / stop_row['count']
+
+        # Check adjacent sections
+        adjacent_sections = sections_df[sections_df.geometry.touches(section_row['geometry'])]
+        for _, adjacent_section_row in adjacent_sections.iterrows():
+            for _, stop_row in stops_df.iterrows():
+                stop = Point(stop_row['stop_lon'], stop_row['stop_lat'])
+                if stop.within(adjacent_section_row['geometry']):
+                    validations_count += stop_row['validations'] / stop_row['count']
 
         centroid_validations_df = centroid_validations_df._append({'section_id': region_id, 'validations': validations_count}, ignore_index=True)
     
@@ -68,22 +75,26 @@ def main():
             print(f'> Validations CSV file path {validations_path} does not exist')
             continue
 
+        print(f'> Opening stops.txt GTFS file in {gtfs_path} DIR')
         stops_path = os.path.join(gtfs_path, 'stops.txt')
 
         if not os.path.exists(stops_path):
             print(f'> stops.txt file in DIR {gtfs_path} does not exist')
             continue
 
+        print(f'> Opening validations file {validations_path}')
         validations_df = pd.read_csv(validations_path)
         stops_df = pd.read_csv(stops_path)
         stops_df['stop_name'] = stops_df['stop_name'].apply(unidecode)
 
         centroid_validations.append(group_validations_centroids(sections_df, validations_df, stops_df))
 
+    print(f'> Merging centroid validations')
     merged_df = pd.concat(centroid_validations).groupby(['section_id']).sum().reset_index()
     merged_df['section_id'] = merged_df['section_id'].astype('int64')
     merged_df['validations'] = merged_df['validations'].astype('int64')
 
+    print(f'> Writing results to {output_path}')
     merged_df.to_csv(output_path, index=False)
 
 
