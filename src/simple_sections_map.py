0d56f3e759762222c015fd49c1ee233e8afe6122
francisco-rente <up201906761@up.pt>
2024-01-03 15:03:02 +0000

    feat: map plotting with same scale and storing in maps

--- src/simple_sections_map.py
+++ src/simple_sections_map.py
@@ -38,58 +38,45 @@ def read_datasets():
     return df.merge(val_per_section, left_on='OBJECTID', right_on='section_id'), df.merge(val_per_section_without_trindade, left_on='OBJECTID', right_on='section_id')
 
 
-def plot_map(df, metric, title, img_name):
+def plot_map(df, metric, title, img_name, min_val=0, max_val=1):
     df = df.to_crs(epsg=3857)
     ax = df.eval(f'pct_afam = {metric}')\
-            .plot('pct_afam', cmap='plasma', alpha=.7, linewidth=.25, edgecolor='k', legend=True)
+            .plot('pct_afam', cmap='plasma', alpha=.7, linewidth=.15, edgecolor='k', legend=True, vmin = min_val, vmax = max_val,
+                                        legend_kwds={'orientation': "horizontal", 
+                                                    'shrink': 0.8,
+                                                     }, figsize=(10, 10))
     ax.set_axis_off()
     ax.set_title(title)
 
     ctx.add_basemap(ax, source=ctx.providers.CartoDB.Positron)
     
-    # plt.savefig(os.path.join(MAPS, img_name + ".png"), dpi=300)
-    plt.show()
+
+    plt.savefig(os.path.join(MAPS, img_name + ".png"), dpi=500, bbox_inches='tight')
+    plt.close()
 
 
 def main():
+    print("Reading datasets")
     df, df_without_trindade = read_datasets()
+
+    if not os.path.exists(MAPS):
+        try:
+            print(f"Creating directory {MAPS}")
+            os.mkdir(MAPS)
+        except OSError:
+            print ("Creation of the directory %s failed" % MAPS)
+            exit(1)
+
     print("Plotting maps")
-
-    plot_map(df, "indicator_norm", "Norma indicador", "norma_indicador")
-    plot_map(df_without_trindade, "indicator_norm", "Norma indicador sem Trindade", "norma_indicador_sem_trindade")
-
-    plot_map(df, "validations", "validations", "validations")
-    plot_map(df_without_trindade, "validations", "validations_sem_trindade", "validations_sem_trindade")
-
-
-    plot_map(df, "N_INDIVIDUOS", "No individuos", "No individuos")
-    plot_map(df, "weight", "Peso", "Peso")
-
-
-
-    # print("Plotting validations")
-    # plot_map(df, "validations", "validations")
-
-    # metric_in_geopkg = ['N_NUCLEOS_FAMILIARES','N_NUCLEOS_FAMILIARES_COM_FILHOS_TENDO_O_MAIS_NOVO_MENOS_DE_25',\
-    #    'N_INDIVIDUOS', 'N_INDIVIDUOS_H', 'N_INDIVIDUOS_M', 'N_INDIVIDUOS_0_14',\
-    #    'N_INDIVIDUOS_15_24', 'N_INDIVIDUOS_25_64', 'N_INDIVIDUOS_65_OU_MAIS','N_ALOJAMENTOS_TOTAL', 
-    #     'N_EDIFICIOS_EXCLUSIV_RESID', 'N_ALOJAMENTOS_FAMILIARES', 'N_ALOJAMENTOS_FAM_CLASS_RHABITUAL',\
-    #     'N_RHABITUAL_COM_ESTACIONAMENTO'
-    #     ]
+    plot_map(df, "indicator_norm", "Normalized indicator per section", "norma_indicador")
+    plot_map(df_without_trindade, "indicator_norm", "Normalized indicator w/o Trindade", "norma_indicador_sem_trindade")
     
+
+    plot_map(df, "validations", "No of validations", "validations", min_val=0, max_val=10**6)
+    plot_map(df_without_trindade, "validations", "Validations w/o Trindade", "validations_sem_trindade", min_val=0, max_val=10**6)
     
-    # # for each metric, divid indicator by the norm of the metric and plot 
-    # for metric in metric_in_geopkg: 
-    #     # get the norm 
-    #     metric_mean = df[metric].mean()
-    #     metric_std = df[metric].std()
-    #     temp_df = df.copy()
-    #     temp_df[f"{metric}_norm"] = (df[metric] - metric_mean) / metric_std
-    #     #temp_df[f"{metric}_norm"] = temp_df.apply(lambda x: x[f"{metric}_norm"]/ x["indicator_norm"] if x["indicator_norm"] > 0 else 0, axis=1)
-    #     temp_df[f"{metric}_norm"] = temp_df.apply(lambda x: x["indicator"] / x[f"{metric}_norm"] if x[f"{metric}_norm"] != 0 else 0, axis=1) 
-    #     plot_map(temp_df, f"{metric}_norm", f"indicator_norm_over_{metric}")
-
-
+    plot_map(df, "weight", "Weight per section", "weight", min_val=400, max_val=1500)
+    plot_map(df_without_trindade, "weight", "Weight w/o Trindade", "weight_sem_trindade", min_val=400, max_val=1500)
        
 
 
